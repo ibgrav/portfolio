036ec893edba5375bf146fefd2be43d598a9d924
Isaac Graves <igraves@hbs.edu>
2023-10-29 18:58:53 +0100

    Initialize CDK Deploy

---
+++ bin/cdk.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { CdkStack } from "./stack.js";
+
+const app = new cdk.App();
+
+const region = process.env.AWS_REGION;
+const account = process.env.AWS_ACCOUNT;
+
+if (!region || !account) throw new Error(`Missing AWS_REGION or AWS_ACCOUNT`);
+
+new CdkStack(app, "CdkStack", {
+  env: { region, account }
+});
