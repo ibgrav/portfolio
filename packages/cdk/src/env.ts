f984b5836affbe0be276c0ade1050eee55822491
Isaac Graves <igraves@hbs.edu>
2023-10-29 16:20:56 +0100

    clean

--- packages/cdk/src/env.ts
+++
@@ -1,8 +0,0 @@
-export const BRANCH = process.env.GIT_BRANCH || "main";
-
-export const AWS_PROFILE = process.env.AWS_PROFILE;
-export const AWS_ACCOUNT = process.env.AWS_ACCOUNT;
-export const AWS_REGION = process.env.AWS_REGION;
-
-export const STATIC_SITE_STACK_NAME = "StaticSiteStack";
-export const PUBLIC_BUCKET_OUTPUT_KEY = "PublicBucketName";
--- readme.md
+++
@@ -1,5 +0,0 @@
-# Portfolio
-
-1. List relevant projects
-2. Build each with tech used in the project (if possible)
-3. Show the code live in real-time (a REPL)
