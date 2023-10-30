c3cceacffcca7ea89dbdbc77ffe7235936cad026
Isaac Graves <igraves@hbs.edu>
2023-10-30 21:35:28 +0100

    print fn url

--- bin/stack/stack.ts
+++ bin/stack/stack.ts
@@ -1,6 +1,6 @@
 import { join } from "path";
 import type { Construct } from "constructs";
-import { RemovalPolicy, Stack, aws_route53_targets as targets } from "aws-cdk-lib";
+import { CfnOutput, RemovalPolicy, Stack, aws_route53_targets as targets } from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as api from "aws-cdk-lib/aws-apigateway";
@@ -18,14 +18,22 @@ export class PortfolioStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
+    const bucket = new s3.Bucket(this, name("Bucket"), {
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
     const fn = new NodejsFunction(this, name("Function"), {
       handler: "handler",
       entry: "dist/server/lambda.js",
       runtime: lambda.Runtime.NODEJS_18_X
     });
 
-    const { url } = fn.addFunctionUrl({
+    const fnUrl = fn.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE
+    });
+
+    new CfnOutput(this, name("FunctionUrl"), {
+      value: fnUrl.url
     });
 
     const zone = new route53.HostedZone(this, "HostedZone", {
@@ -43,17 +51,19 @@ export class PortfolioStack extends Stack {
       priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
       httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
       defaultBehavior: {
-        origin: new origins.HttpOrigin(url.replace("https://", ""))
+        // origin: new origins.HttpOrigin(fnUrl.url.replace("http://", ""))
+        origin: new origins.S3Origin(bucket)
+      },
+      additionalBehaviors: {
+        "/assets/*": {
+          origin: new origins.S3Origin(bucket)
+        }
       }
     });
 
     new route53.ARecord(this, name("ARecord"), {
       zone: zone,
       target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
-    });
-
-    const bucket = new s3.Bucket(this, name("Bucket"), {
-      removalPolicy: RemovalPolicy.DESTROY
     });
 
     new s3Deployment.BucketDeployment(this, name("BucketDeployment"), {
