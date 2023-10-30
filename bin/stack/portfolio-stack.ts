e63626039051763e9d867b32d9ee9a6dcd278b6f
Isaac Graves <igraves@hbs.edu>
2023-10-30 09:58:25 +0100

    add api gateway

--- bin/stack/portfolio-stack.ts
+++ bin/stack/portfolio-stack.ts
@@ -4,6 +4,7 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3_deployment from "aws-cdk-lib/aws-s3-deployment";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as api from "aws-cdk-lib/aws-apigateway";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
@@ -22,22 +23,22 @@ export class PortfolioStack extends Stack {
       destinationBucket: bucket
     });
 
-    const edgeFn = new NodejsFunction(this, name("Function"), {
+    const fn = new NodejsFunction(this, name("Function"), {
       handler: "handler",
       entry: "dist/server/lambda.js",
       runtime: lambda.Runtime.NODEJS_18_X
     });
 
+    fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE
+    });
+
+    const gateway = new api.LambdaRestApi(this, name("Api"), { handler: fn });
+
     new cloudfront.Distribution(this, name("Distribution"), {
       httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
       defaultBehavior: {
-        origin: new origins.S3Origin(bucket),
-        edgeLambdas: [
-          {
-            functionVersion: edgeFn.currentVersion,
-            eventType: cloudfront.LambdaEdgeEventType.VIEWER_REQUEST
-          }
-        ]
+        origin: new origins.RestApiOrigin(gateway)
       }
     });
   }
