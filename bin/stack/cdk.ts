725d8b762e8bb09e90c08c221c4ceced9fceff07
Isaac Graves <igraves@hbs.edu>
2023-10-30 19:09:25 +0100

    join staacks and add custom domain

--- bin/stack/cdk.ts
+++ bin/stack/cdk.ts
@@ -1,13 +1,22 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { PortfolioStack } from "./portfolio-stack.js";
+import { PortfolioStack } from "./stack.js";
+
+export const name = (id: string) => "Portfolio" + id;
+export type StackProps = cdk.StackProps & typeof props;
 
 const app = new cdk.App();
 
-new PortfolioStack(app, "PortfolioStack", {
+const props = {
   domainName: process.env.DOMAIN_NAME!,
   env: {
     region: process.env.AWS_REGION!,
     account: process.env.AWS_ACCOUNT!
   }
-});
+};
+
+if (!props.domainName) throw new Error("domainName is required");
+if (!props.env.region) throw new Error("env region is required");
+if (!props.env.account) throw new Error("env account is required");
+
+new PortfolioStack(app, name("Stack"), props);
--- bin/stack/stack.ts
+++ bin/stack/stack.ts
@@ -1,35 +1,28 @@
 import { join } from "path";
 import type { Construct } from "constructs";
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, RemovalPolicy, Stack, aws_route53_targets as targets } from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as route53 from "aws-cdk-lib/aws-route53";
 import * as api from "aws-cdk-lib/aws-apigateway";
-import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as route53 from "aws-cdk-lib/aws-route53";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { StackProps, name } from "./cdk.js";
 
 const dist = join(process.cwd(), "dist/client");
-const name = (tag: string) => "Portfolio" + tag;
-
-interface PortfolioStackProps extends StackProps {
-  domainName: string;
-  env: Required<StackProps["env"]>;
-}
 
 export class PortfolioStack extends Stack {
-  constructor(scope: Construct, id: string, props?: PortfolioStackProps) {
+  constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-
-    if (!props?.domainName) throw new Error("domainName is required");
-    if (!props?.env?.region) throw new Error("env region is required");
-    if (!props?.env?.account) throw new Error("env account is required");
 
     /* STATIC ASSETS */
 
-    const bucket = new s3.Bucket(this, name("Bucket"));
+    const bucket = new s3.Bucket(this, name("Bucket"), {
+      removalPolicy: RemovalPolicy.DESTROY
+    });
 
     new s3Deployment.BucketDeployment(this, name("BucketDeployment"), {
       sources: [s3Deployment.Source.asset(dist)],
@@ -50,24 +43,35 @@ export class PortfolioStack extends Stack {
 
     const gateway = new api.LambdaRestApi(this, name("Api"), { handler: fn });
 
-    /* DOMAIN NAME */
+    /* CLOUDFRONT */
 
     const zone = new route53.HostedZone(this, "HostedZone", {
       zoneName: props.domainName
     });
 
-    new acm.Certificate(this, name("Certificate"), {
+    const certificate = new acm.Certificate(this, "PortfolioCertificate", {
       domainName: props.domainName,
       validation: acm.CertificateValidation.fromDns(zone)
     });
 
-    /* CLOUDFRONT */
-
-    new cloudfront.Distribution(this, name("Distribution"), {
+    const distribution = new cloudfront.Distribution(this, name("Distribution"), {
+      certificate: certificate,
+      domainNames: [props.domainName],
+      priceClass: cloudfront.PriceClass.PRICE_CLASS_100,
       httpVersion: cloudfront.HttpVersion.HTTP2_AND_3,
       defaultBehavior: {
         origin: new origins.RestApiOrigin(gateway)
       }
+    });
+
+    const record = new route53.ARecord(this, name("ARecord"), {
+      zone: zone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
+    });
+
+    new CfnOutput(this, name("OutputDomainName"), {
+      value: record.domainName,
+      exportName: "aRecordDomainName"
     });
   }
 }
