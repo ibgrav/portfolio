6d94289c7922e52f4a1f26e22f70c5f8ab5e6e1b
Isaac Graves <igraves@hbs.edu>
2023-10-30 08:50:31 +0100

    add s3 bucket deployment and vite

--- bin/stack.ts
+++ bin/stack.ts
@@ -1,16 +1,24 @@
+import { join } from "path";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+
+const name = "Portfolio";
+const dist = join(process.cwd(), "dist");
 
 export class PortfolioStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new cdk.aws_s3.Bucket(this, "PortfolioBucket");
+    const bucket = new cdk.aws_s3.Bucket(this, name);
 
-    const cloudfront = new cdk.aws_cloudfront.Distribution(this, "PortfolioDistribution", {
-      defaultBehavior: { origin: new cdk.aws_cloudfront_origins.S3Origin(bucket) }
+    new cdk.aws_s3_deployment.BucketDeployment(this, name, {
+      sources: [cdk.aws_s3_deployment.Source.asset(dist)],
+      destinationBucket: bucket
     });
 
-    console.log(cloudfront.distributionDomainName);
+    new cdk.aws_cloudfront.Distribution(this, name, {
+      httpVersion: cdk.aws_cloudfront.HttpVersion.HTTP2_AND_3,
+      defaultBehavior: { origin: new cdk.aws_cloudfront_origins.S3Origin(bucket) }
+    });
   }
 }
